42d76c2d203a8b888a35fd05811dbc1509d1b62a
unknown <ptoroar@gmail.com>
2024-11-29 23:05:23 -0300

    [CHG] se resuelve conflicto

--- menu_principal.py
+++ menu_principal.py
@@ -4,10 +4,17 @@ import constantes
 def pantalla_inicio(ventana):
     letras_titulo = pygame.font.Font(None, 74)
     letras_botones = pygame.font.Font(None, 36)
+    letras_instrucciones = pygame.font.Font(None, 24) # Fuente para las instrucciones
 
     titulo = letras_titulo.render("Bienvenido a CRECE", True, constantes.COLOR_BLANCO)
     texto_jugar = letras_botones.render("Iniciar", True, constantes.COLOR_NEGRO)
     texto_salir = letras_botones.render("Salir", True, constantes.COLOR_NEGRO)
+
+
+    # Instrucciones
+    instrucciones_movimiento = letras_instrucciones.render("Usa las flechas para moverte", True, constantes.COLOR_BLANCO)
+    instrucciones_interaccion = letras_instrucciones.render("Presiona 'E' para sumar un dia de consumo", True, constantes.COLOR_BLANCO)
+    instrucciones_mensaje = letras_instrucciones.render("¡Aprendamos a reducir el consumo eléctrico en casa!", True, constantes.COLOR_VERDE)
 
     boton_jugar = pygame.Rect(constantes.ANCHO_VENTANA / 2 - 100, 
                             constantes.ALTO_VENTANA / 2 - 50, 200, 50)
@@ -42,5 +49,13 @@ def pantalla_inicio(ventana):
                                 boton_jugar.y + boton_jugar.height / 2 - texto_jugar.get_height() / 2))
         ventana.blit(texto_salir, (boton_salir.x + boton_salir.width / 2 - texto_salir.get_width() / 2,
                                 boton_salir.y + boton_salir.height / 2 - texto_salir.get_height() / 2))
+        
+        # Dibujar las instrucciones
+        ventana.blit(instrucciones_movimiento, (constantes.ANCHO_VENTANA / 2 - instrucciones_movimiento.get_width() / 2, 
+                                                constantes.ALTO_VENTANA / 2 + 150))
+        ventana.blit(instrucciones_interaccion, (constantes.ANCHO_VENTANA / 2 - instrucciones_interaccion.get_width() / 2, 
+                                                 constantes.ALTO_VENTANA / 2 + 180))
+        ventana.blit(instrucciones_mensaje, (constantes.ANCHO_VENTANA / 2 - instrucciones_mensaje.get_width() / 2, 
+                                             constantes.ALTO_VENTANA / 2 + 210))
 
         pygame.display.flip()
